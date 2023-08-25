2b8f8f3bcada3d8732391a7422ec6d165cd3c640
erali <eraliergashev875@gmail.com>
2023-08-25 12:49:23 +0500

    sss

---
+++ script.js
@@ -0,0 +1,57 @@
+
+// function timeSecund(seconds) {
+//     const hours = Math.floor(seconds / 3600);
+  
+//     const remainingSeconds = seconds % 3600;
+  
+//     return `${hours} - soat va ${remainingSeconds} - sekund o'tdi`;
+//   }
+  
+
+
+//   const totalSeconds = 12321;
+//   console.log(timeSecund(totalSeconds));
+  
+// function numberDivisors(number) {
+//     const divisors = [];
+  
+//     for (let i = 1; i <= number; i++) {
+//       if (number % i === 0) {
+//         divisors.push(i);
+//       }
+//     }
+  
+//     return divisors;
+//   }
+  
+//   const number = 24;
+//   console.log(numberDivisors(number));
+  
+
+
+
+//   function caseNew(k){
+//     switch(k){
+//         case 1:
+//             console.log("yomon");
+//             break;
+//         case 2:
+//             console.log("qoniqarsiz");
+//         break;
+//         case 3:
+//             console.log("qoniqarli");
+//             break;
+//             case 4:
+//                 console.log("yaxshi");
+//                 break;
+//                 case 5:
+//                     console.log("alo");
+//                     break;
+//     }
+//   }
+//   console.log(caseNew(5));
+ 
+
+// scope turlari 3 ga bo'linadi global function blog blog ga for while do while functionga funksiya globalga if else
+
+// javaScript 1995-yilda yaratilgan 1995-yilda veb-brauzerlarda ishlay boshladi. 2009-yilda Backend qo'shildi
